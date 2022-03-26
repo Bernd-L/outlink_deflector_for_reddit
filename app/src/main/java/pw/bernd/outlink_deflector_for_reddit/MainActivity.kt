7d3f8fa850797dabec40e2c7d148e3f766d2661e
Bernd-L <git@bernd.pw>
2022-03-26 19:35:24 +0100

    Show the entire URL in a toast

--- app/src/main/java/pw/bernd/outlink_deflector_for_reddit/MainActivity.kt
+++ app/src/main/java/pw/bernd/outlink_deflector_for_reddit/MainActivity.kt
@@ -1,11 +1,25 @@
 package pw.bernd.outlink_deflector_for_reddit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.appcompat.app.AppCompatActivity
+
+
+const val TAG = "deflector_dbg";
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        val data = intent.data
+        val urlString = data.toString()
+        Log.e(TAG, urlString)
+
+
+        Toast.makeText(applicationContext, "Got whole URL: $urlString", LENGTH_SHORT).show()
+//        exitProcess(0)
     }
 }
